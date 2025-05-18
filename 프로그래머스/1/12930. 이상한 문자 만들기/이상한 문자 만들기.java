5496f227f162ca93d06891fcbc2414d0aeb10633
minjun <116476333+Jominjun1@users.noreply.github.com>
2025-05-19 08:57:09 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 14.13 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) != ' '){
+                if (count % 2 == 1) {
+                    answer += Character.toLowerCase(s.charAt(i));
+                } else {
+                    answer += Character.toUpperCase(s.charAt(i));
+                }
+                count++;
+            } else {
+                answer += " ";
+                count = 0;
+            }
+        }
+        return answer;
+    }
+}
