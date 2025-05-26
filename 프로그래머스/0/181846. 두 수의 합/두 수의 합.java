d33fa55c664eea47de0dffa64596cfe14bbe3342
minjun <116476333+Jominjun1@users.noreply.github.com>
2025-05-26 14:16:37 +0900

    [level 0] Title: 두 수의 합, Time: 515.80 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,8 @@
+import java.math.*;
+class Solution {
+    public String solution(String a, String b) {
+        BigInteger bigNumber1 = new BigInteger(a);
+        BigInteger bigNumber2 = new BigInteger(b);
+        return bigNumber1.add(bigNumber2).toString();
+    }
+}
